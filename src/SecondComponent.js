b149916f6ff321a74f4ac205e71fee403229bff6
aaron-portuphy <72166690+aaron-portuphy@users.noreply.github.com>
2020-10-10 01:44:38 +0000

    SecondComponent

---
+++ src/SecondComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SecondComponent(){
+    return(
+        <img src="Virtual Education Fair 2020.jpg">
+    
+        </img>
+    )
+}
+
+export default SecondComponent;
